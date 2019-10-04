db1c9d43ac0612b7e09ac97f4d89698905789d2c
Geraldine Romero <geraldine.romero@softvision.com>
2019-10-04 12:44:23 -0300

    Dependency added + change in main class

--- src/main/java/com/softvision/training/config/AppConfig.java
+++ src/main/java/com/softvision/training/config/AppConfig.java
@@ -1,12 +1,17 @@
 package com.softvision.training.config;
 
+import javafx.application.Application;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.SpringApplication;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.softvision.training"})
 public class AppConfig {
 
+    public static void main (String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
 }
