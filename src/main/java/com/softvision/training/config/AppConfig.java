36232db7643ff08ba3cf93eec7910670667a72ef
Geraldine Romero <geraldine.romero@softvision.com>
2019-10-04 14:51:47 -0300

    Pending to fix main class

--- src/main/java/com/softvision/training/config/AppConfig.java
+++ src/main/java/com/softvision/training/config/AppConfig.java
@@ -1,13 +1,12 @@
 package com.softvision.training.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.softvision.training"})
 public class AppConfig {
 
     public static void main(String[] args) {
